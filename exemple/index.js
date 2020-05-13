e33ff15481f7998b69a5400cb817a3977a5e2757
Sazzo <stolensoul32@gmail.com>
2020-05-13 19:49:17 -0300

    Remove the exemple bot token :v

--- exemple/index.js
+++ exemple/index.js
@@ -1,6 +1,6 @@
 const Mooe = require('../lib/Mooe.js')
 
-const client = new Mooe('Njk1NjM5Mzk3OTEwOTcwNTg4.Xrxenw.N7-keh8LVwkdSeDhB8Nb4d_pWW0')
+const client = new Mooe('BOT_TOKEN')
 
 client.connect()
 console.log('connected!')
