04ed250459205f5c31bec047f3227f19cd852420
Sazzo <stolensoul32@gmail.com>
2020-05-13 21:10:33 -0300

    Added heartbeat function

--- exemple/index.js
+++ exemple/index.js
@@ -1,6 +1,6 @@
 const Mooe = require('../lib/Mooe.js')
 
-const client = new Mooe('BOT_TOKEN')
+const client = new Mooe('Njk1NjM5Mzk3OTEwOTcwNTg4.XryDPw.QWPABbx7tywxIrU4jVSkab2x5Yg')
 
 client.connect()
 console.log('connected!')
